13e95c8c94d0193b5f6274c531fbaa5239e595e3
TanHuynh68 <tanhpce171112@fpt.edu.vn>
2024-09-16 02:17:10 +0700

    video in home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -12,15 +12,11 @@ const HomePage = () => {
             <div className="relative overflow-hidden w-full h-[500px] bg-gray-900">
                 <iframe
                     className="absolute top-0 left-0 w-full h-full"
-                    src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1"
+                    src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&loop=1&playlist=dQw4w9WgXcQ"
                     title="YouTube video player"
-                    frameBorder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowFullScreen
                 ></iframe>
-                {/* <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50">
-                    <Title level={2} className="text-white">Welcome to Our Site</Title>
-                </div> */}
             </div>
             <FooterComponent />
         </div>
