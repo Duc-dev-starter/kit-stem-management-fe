4affd08783798efd318ff8584f18eef3b72756ce
Yumi Trần <tranleyumi113@gmail.com>
2024-09-26 01:25:20 +0700

    Header.css

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -1,33 +1,93 @@
-import { MenuOutlined, ShoppingCartOutlined, UserOutlined } from '@ant-design/icons';
-import { Col, Image, Row } from 'antd';
-const Navbar = () => {
+import {
+    ShoppingCartOutlined,
+    UserOutlined,
+  } from "@ant-design/icons";
+  import { Col, Image, Row, Typography } from "antd";
+  import './navbar.css'; // Import CSS for hover effects
+  
+  const { Text } = Typography;
+  
+  const Navbar = () => {
     return (
-        <>
-            <div className='flex justify-between items-center p-3 bg-gray-300'>
-                <MenuOutlined
-                className='cursor-pointer'
-                />
-                <div className=''>
-                <Image 
-                    width={125}
-                    src="https://www.crunchlabs.com/cdn/shop/files/dark-logo.svg?v=1676481560&width=500"
-                />
-                </div>
-                <Row gutter={10}>
-                    <Col>
-                        <UserOutlined
-                        className='cursor-pointer'
-                        />
-                    </Col>
-                    <Col>
-                        <ShoppingCartOutlined 
-                        className='cursor-pointer'
-                        />
-                    </Col>
-                </Row>
-            </div>
-
-        </>
-    )
-}
-export default Navbar;
+      <Row
+        className="navbar"
+        justify="space-between"
+        align="middle"
+        style={{
+          padding: "0px 200px", 
+          backgroundColor: "#fff", 
+        }}
+      >
+        {/* Left Links Section */}
+        <Col xs={24} sm={12} md={8} lg={6} style={{ display: "flex", justifyContent: "center" }}>
+          <Row gutter={20} align="middle" style={{marginLeft: "0px"}}>
+            <Col style={{ padding: '28px 18px' }}>
+              <Text style={{ color: "black", textDecoration: "none" }}>
+                <a href="#" className="navbar-link"> {/* Updated to use class */}
+                  Subscription Boxes
+                </a>
+              </Text>
+            </Col>
+            <Col style={{ padding: '28px 18px' }}>
+              <Text>
+                <a href="#" className="navbar-link">
+                  Our Company
+                </a>
+              </Text>
+            </Col>
+            <Col style={{ padding: '28px 18px' }}>
+              <Text>
+                <a href="#" className="navbar-link">
+                  Shop
+                </a>
+              </Text>
+            </Col>
+          </Row>
+        </Col>
+  
+        {/* Center Logo */}
+        <Col
+          xs={24}
+          sm={12}
+          md={8}
+          lg={6}
+          className="logo-container"
+          style={{ textAlign: "center" }}
+        >
+          <Image
+            width={125}
+            src="https://www.crunchlabs.com/cdn/shop/files/dark-logo.svg?v=1676481560&width=500"
+            alt="Logo"
+          />
+        </Col>
+  
+        {/* Right Icons Section */}
+        <Col xs={24} sm={12} md={8} lg={6} style={{ display: "flex", justifyContent: "flex-end" }}>
+          <Row gutter={20} align="middle">
+            <Col style={{ padding: '28px 18px' }}>
+              <Text>
+                <a href="#" className="navbar-link">
+                  Schools & Groups
+                </a>
+              </Text>
+            </Col>
+            <Col>
+              <UserOutlined
+                className="navbar-icon cursor-pointer logo-user"
+                style={{ fontSize: "24px", color: "black", textDecoration: "none" }}
+              />
+            </Col>
+            <Col>
+              <ShoppingCartOutlined
+                className="navbar-icon cursor-pointer shopping-cart"
+                style={{ fontSize: "24px", color: "black", textDecoration: "none" }}
+              />
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    );
+  };
+  
+  export default Navbar;
+  
