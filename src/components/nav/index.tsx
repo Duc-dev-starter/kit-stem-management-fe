0b67395344dab2fa543be8fbbc5f76744c8712ac
TanHuynh68 <155423109+TanHuynh68@users.noreply.github.com>
2024-10-30 19:03:58 +0700

    home page

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -4,12 +4,13 @@ import "./navbar.css"; // Import CSS for hover effects
 import { useNavigate } from "react-router-dom";
 import ShopDropDown from "./shop";
 import DropdownAvatar from "../dropdown/DropdownAvatar";
-import { user } from "../../services";
 import { useEffect, useState } from "react";
 import CustomerCart from "./cart";
 import { getCartsService } from "../../services/cart.services";
 import { Cart } from "../../models/Cart.model";
 import { imageTemp } from "../../consts/others";
+import { getUserFromLocalStorage } from "../../utils";
+import { User } from "../../models";
 
 const { Text } = Typography;
 
@@ -17,9 +18,15 @@ const Navbar = () => {
   const [open, setOpen] = useState(false);
   const [carts, setCarts] = useState<Cart[]>([]);
   const navigate = useNavigate();
+  const [user, setUser] = useState<User>();
+  useEffect(() => {
+    if(user){
+      getCarts();
+    }
+  }, [user])
 
   useEffect(() => {
-    getCarts();
+    getUser()
   }, [])
 
   const showDrawer = () => {
@@ -29,7 +36,10 @@ const Navbar = () => {
   const onClose = () => {
     setOpen(false);
   };
-
+  const getUser = () => {
+    const user = getUserFromLocalStorage();
+    setUser(user);
+  }
   const getCarts = async () => {
     const response = await getCartsService("", "")
     if (response && response.data.pageData) {
@@ -53,11 +63,11 @@ const Navbar = () => {
             </>
           ))
         }
-      <div className="flex justify-center mt-2">
-      <button  type="button" className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
-          Checkout
-        </button>
-      </div>
+        <div className="flex justify-center mt-2">
+          <button type="button" className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+            Checkout
+          </button>
+        </div>
       </Drawer>
       <Row
         className="navbar"
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,6 @@
+import { Col, Image, Row } from "antd";
+import Title from "antd/es/typography/Title";
+import { Link } from "react-router-dom";
 
 const HomePage: React.FC = () => {
 
@@ -13,6 +16,39 @@ const HomePage: React.FC = () => {
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowFullScreen
                 ></iframe>
+            </div>
+            <Title className="text-center mt-2" level={1}>How CrunchLabs Works</Title>
+            <div className="grid grid-cols-3">
+                <div>
+                    <Title level={3} className="font-bold text-center">Build</Title>
+                    <Image src="https://cdn.shopify.com/s/files/1/0634/1535/3575/files/CrunchLabs_HIW_01.png?v=1713021949" />
+                </div>
+                <div>
+                    <Title level={3} className="font-bold text-center">Play and Hack</Title>
+                    <Image src="https://cdn.shopify.com/s/files/1/0634/1535/3575/files/CrunchLabs_HIW_02.png?v=1713021949" />
+                </div>
+                <div>
+                    <Title level={3} className="font-bold text-center">Think</Title>
+                    <Image src="https://cdn.shopify.com/s/files/1/0634/1535/3575/files/CrunchLabs_HIW_03.png?v=1713021949" />
+                </div>
+            </div>
+            <div style={{ backgroundColor: "#00416c" }} className="container mx-auto py-5 mb-10 px-20">
+                <Row>
+                    <Col span={16}>
+                        <Title className="text-white" level={1}>  Thinking like an engineer means you're resilient. It's getting back up after being knocked down and approaching things differently. That determination helps kids tackle everyday challenges and it's the same determination that puts rovers on other dang planets!</Title>
+                        <Title className="text-white" level={5}>- Mark Rober
+                            Founder of CrunchLabs, NASA engineer for the Curiosity Mars Rover, public enemy #1 of neighborhood squirrels, & creator of the sparkliest glitter bomb pranks</Title>
+                       <div className="text-center mt-3">
+                       <Link to={"/about"} >
+                            <button type="button" className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">ABOUT US</button>
+                        </Link>
+                       </div>
+                    </Col>
+                    <Col span={8}>
+                        <img src="https://www.crunchlabs.com/cdn/shop/files/mark-nasa-shirt_1_3x_509ac96a-f1f2-4e7f-a1e8-b83d5b10248c.webp?v=1711497523&width=800" alt="" />
+                    </Col>
+                </Row>
+
             </div>
         </div>
     );
